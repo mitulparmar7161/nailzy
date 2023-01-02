4df4966fd06e68f87110bd00ceeffe4a575c2588
neelpanchal2303 <200233043045@silveroakuni.ac.in>
2023-01-02 17:08:45 +0530

    neel + mitul migration

---
+++ nailzy/database/migrations/2022_12_29_045037_create_booked_services_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booked_services', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('booking_id');
+            $table->unsignedBigInteger('service_id');
+            $table->double('price');
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booked_services');
+    }
+};
